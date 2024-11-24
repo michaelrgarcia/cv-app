c9e7e62de8688c00e788cad7c1278b68bae592b1
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2024-11-23 21:54:40 -0800

    Replace App component w/ Form component + h1

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import Form from "./Form";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <h1>CV App</h1>
+    <Form />
+  </StrictMode>
+);
